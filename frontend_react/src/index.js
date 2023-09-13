2608bd44a2305f1f130dd3804a542e56ee60b512
lucassr-dev <lucassr.job@gmail.com>
2023-09-13 19:49:32 -0300

    update react render for react 18

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { createRoot } from 'react';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+const root = createRoot(document.getElementById('root'), { strict: true });
+
+root.render(
+  <App />
 );
